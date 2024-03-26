4ef1cee7aa6ed9e0548ae471cb6922370174fafc
Vivek Dagar <vivekdagar2023@gmail.com>
2024-03-26 18:43:38 +0530

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xbasic',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'click~=8.1.7',
+        'setuptools~=68.0.0',
+        'webbrowser',
+        'wheel',
+    ],
+    entry_points={
+        'console_scripts': [
+            'xbasic = xbasic.main:cli',
+        ],
+    },
+    author='Vivek Dagar',
+    author_email='vivekdagar2017@gmail.com',
+    description='A custom interpreter for a variant of the BASIC programming language.',
+    url='https://github.com/vivekkdagar/xbasic/',
+    long_description="""Please refer to the Github for usage guide and more {
+    https://github.com/vivekkdagar/xbasic}""",
+    long_description_content_type='text/markdown',
+    license='MIT License',
+)
